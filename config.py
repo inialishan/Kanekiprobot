52d6760188e5dffabcc4744f1ac86fbbc80c616b
AlishanSiddqui <99635375+inialishan@users.noreply.github.com>
2024-04-13 21:46:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/inialishan/DAXXMUSIC",
+    "https://github.com/inialishan/Kanekiprobot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
